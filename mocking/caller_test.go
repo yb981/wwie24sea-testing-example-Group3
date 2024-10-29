fc8d35e918ca99d3a9266b2c7ccb7449f9ac2ceb
aaronschweig <aaron.schweig@gmail.com>
2024-10-29 14:38:32 +0100

    chore: sample tests during lecture

--- mocking/caller_test.go
+++ mocking/caller_test.go
@@ -1,10 +1,30 @@
 package mocking_test
 
 import (
+	"net/http"
 	"testing"
+
+	"github.com/aaronschweig/wwi24sea-testing-example/mocking"
 )
+
+type testStruct struct{}
+
+func (t *testStruct) Get(url string) (*http.Response, error) {
+	return &http.Response{
+		StatusCode: http.StatusOK,
+	}, nil
+}
 
 func TestCaller(t *testing.T) {
 
-	// TODO: write some mocks for the given implementation
+	caller := mocking.NewCaller(&testStruct{})
+
+	res, err := caller.Call("https://google.com")
+	if err != nil {
+		t.Fail()
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Fatal("expected status to be ok")
+	}
 }
--- unittests/functions_test.go
+++ unittests/functions_test.go
@@ -2,8 +2,55 @@ package unittests_test
 
 import (
 	"testing"
+
+	"github.com/aaronschweig/wwi24sea-testing-example/unittests"
 )
 
 func TestAdd(t *testing.T) {
-	// TODO: write some tests for the given implementation
+	for _, test := range []struct {
+		name           string
+		a, b           int
+		expectedResult int
+	}{
+		{
+			name:           "1 + 1 = 2",
+			a:              1,
+			b:              1,
+			expectedResult: 2,
+		},
+		{
+			name:           "1 + -2 = -1",
+			a:              1,
+			b:              -2,
+			expectedResult: -1,
+		},
+	} {
+		out := unittests.Add(test.a, test.b)
+
+		if out != test.expectedResult {
+			t.Errorf("Expected %d, got %d", test.expectedResult, out)
+		}
+	}
+}
+
+func TestReverse(t *testing.T) {
+	for _, test := range []struct {
+		name  string
+		input string
+		out   string
+	}{
+		{
+			name:  "should reverse the string",
+			input: "Hallo",
+			out:   "ollaH",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			reversedString := unittests.Reverse(test.input)
+
+			if reversedString != test.out {
+				t.Errorf("Expected %s, got %s", test.out, reversedString)
+			}
+		})
+	}
 }
